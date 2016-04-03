3e23afcc007b8e48de6bc025e6b89995f5cc5be3
ssavins1 <samavins3@gmail.com>
2016-04-03 16:32:38 -0400

    Minor MainMenu fixes, browseAndPopular and currentlyPlaying not stretch to fit the content field.

--- src/sample/MainMenu.java
+++ src/sample/MainMenu.java
@@ -2,10 +2,15 @@ package sample;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 
 /**
  * Created by user on 4/2/2016.
@@ -30,7 +35,8 @@ public class MainMenu extends VBox {
     /**A button that should prompt the user to enter a admin password
      * and if it is a valid password, switch the screen to the admin menus.
      */
-    Button adminButton = new Button("Admin");
+    Image adminCog = new Image(getClass().getResourceAsStream("Images/cogs_icon.png"));
+    Button adminButton = new Button("", new ImageView(adminCog));
 
     /**Stores both the menuTitle, and banner*/
     VBox titleAndBanner = new VBox();
@@ -45,7 +51,7 @@ public class MainMenu extends VBox {
     Button browseButton = new Button("Browse for song");
 
     /**Should display the most popular songs on the jukeMeister.*/
-    TabPane popular = new TabPane();
+    TabPane popularSelections = new TabPane();
 
     /**will contain currentlyPlayingImage, currentlyPlayingInfo, and songQueue
      * in that order.
@@ -56,6 +62,9 @@ public class MainMenu extends VBox {
     public MainMenu(){
         this.getChildren().addAll(titleAndBanner, content);
         content.getChildren().addAll(adminButton, browseAndPopular, currentlyPlaying);
+        content.setHgrow(browseAndPopular, Priority.ALWAYS);
+        content.setHgrow(currentlyPlaying, Priority.ALWAYS);
+        this.setVgrow(content, Priority.ALWAYS);
 
         setBannerAndTitle();
         setBrowseAndPopular();
@@ -65,28 +74,45 @@ public class MainMenu extends VBox {
     private void setBannerAndTitle(){
         //Temp cs style change to allow me to see the width of the banner and title*/
         titleAndBanner.setStyle("-fx-background-color: #336699;");
-        titleAndBanner.getChildren().add(menuTitle);
-        titleAndBanner.getChildren().add(banner);
+        titleAndBanner.getChildren().addAll(menuTitle, banner);
         titleAndBanner.setAlignment(Pos.CENTER);
-        titleAndBanner.setMinWidth(this.getWidth());
     }
 
 
     private void setBrowseAndPopular(){
+        Rectangle popularContent = new Rectangle(200, 200, Color.LIGHTSTEELBLUE);
+        Tab popularTab = new Tab("Popular");
+        popularTab.setContent(popularContent);
+        popularTab.setClosable(false);
+        popularSelections.getTabs().add(popularTab);
+
+        browseButton.setPrefWidth(popularContent.getWidth());
         //Add the components to the browseAndPopular VBox*/
-        browseAndPopular.getChildren().addAll(popular, browseButton);
+        browseAndPopular.getChildren().addAll(popularSelections, browseButton);
+        popularSelections.setMinWidth(this.getWidth()/2);
+
     }
 
 
     private void setCurrentlyPlaying(){
+
         //2D rectangle that serves as the view size for the currently playing image
         Rectangle2D viewportRect = new Rectangle2D(100, 100, 100, 100);
         //change the viewport to the rectangle
         currentlyPlayingImage.setViewport(viewportRect);
+        currentlyPlayingImage.setPreserveRatio(true);
+        currentlyPlayingImage.setSmooth(true);
+        currentlyPlayingImage.setCache(true);
+
+        Image dummyAlbumCover = new Image(getClass().getResourceAsStream("Images/display.png"));
+        currentlyPlayingImage.setImage(dummyAlbumCover);
         //Add all the components to the currently playing VBox*/
         currentlyPlaying.getChildren().addAll(currentlyPlayingImage, currentlyPlayingInfo, songQueue);
+
+        currentlyPlaying.setPrefHeight(browseAndPopular.getHeight());
         //Temp css color change to currently playing Image to see it on the screen
         currentlyPlayingImage.setStyle("-fx-background-color: #993399;");
+
     }
 
 
