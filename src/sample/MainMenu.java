2d933c287dc7d0f996ac71d4a55c2714c4a0b3ea
ssavins1 <samavins3@gmail.com>
2016-04-10 10:49:44 -0400

    Switched the main menu from using a series of nested HBoxs and VBoxs to a border pane. Added padding between components. Button needs to be resized and can't seem to get the components to line up the way i want them to.

--- src/sample/MainMenu.java
+++ src/sample/MainMenu.java
@@ -1,12 +1,12 @@
 package sample;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -15,87 +15,92 @@ import javafx.scene.shape.Rectangle;
 /**
  * Created by user on 4/2/2016.
  */
-public class MainMenu extends VBox {
+public class MainMenu extends BorderPane {
 
-    /**Displays the album art of the currently playing song*/
+    /**
+     * Displays the album art of the currently playing song
+     */
     ImageView currentlyPlayingImage = new ImageView();
 
-    /**Will display any info on the song that is avaliable*/
+    /**
+     * Will display any info on the song that is avaliable
+     */
     Label currentlyPlayingInfo = new Label("This will display any info on the song");
 
-    /**Just shows that this is the main menu*/
+    /**
+     * Just shows that this is the main menu
+     */
     Label menuTitle = new Label("Main Menu");
 
-    /**A banner that will display*/
+    /**
+     * A banner that will display
+     */
     Label banner = new Label("Banner");
 
-    /**Should display all the songs in the queue*/
+    /**
+     * Should display all the songs in the queue
+     */
     ScrollPane songQueue = new ScrollPane();
 
-    /**A button that should prompt the user to enter a admin password
+    /**
+     * A button that should prompt the user to enter a admin password
      * and if it is a valid password, switch the screen to the admin menus.
      */
     Image adminCog = new Image(getClass().getResourceAsStream("Images/cogs_icon.png"));
     Button adminButton = new Button("", new ImageView(adminCog));
 
-    /**Stores both the menuTitle, and banner*/
-    VBox titleAndBanner = new VBox();
+    /**
+     * Holds the admin button, browseAndPopular, and currently playing in that order
+     */
+    BorderPane content = new BorderPane();
 
-    /**Holds the admin button, browseAndPopular, and currently playing in that order*/
-    HBox content = new HBox();
 
-    /**Contains the popular tabPane*/
-    VBox browseAndPopular = new VBox();
-
-    /**Should display the browse songs screen*/
+    /**
+     * Should display the browse songs screen
+     */
     Button browseButton = new Button("Browse for song");
 
-    /**Should display the most popular songs on the jukeMeister.*/
-    TabPane popularSelections = new TabPane();
-
-    /**will contain currentlyPlayingImage, currentlyPlayingInfo, and songQueue
-     * in that order.
+    /**
+     * Should display the most popular songs on the jukeMeister.
      */
-    VBox currentlyPlaying = new VBox();
+    ScrollPane popularSelections = new ScrollPane();
+
+    VBox titleAndBanner = new VBox();
+    VBox popularAndBrowse = new VBox();
+    VBox currentlyPlayingPortion = new VBox();
 
 
-    public MainMenu(){
-        this.getChildren().addAll(titleAndBanner, content);
-        content.getChildren().addAll(adminButton, browseAndPopular, currentlyPlaying);
-        content.setHgrow(browseAndPopular, Priority.ALWAYS);
-        content.setHgrow(currentlyPlaying, Priority.ALWAYS);
-        this.setVgrow(content, Priority.ALWAYS);
-
-        setBannerAndTitle();
-        setBrowseAndPopular();
-        setCurrentlyPlaying();
+    public MainMenu() {
+        setTopComponenets();
+        setLeftComponents();
+        setCenterComponenets();
+        setRightComponenets();
     }
 
-    private void setBannerAndTitle(){
-        //Temp cs style change to allow me to see the width of the banner and title*/
-        titleAndBanner.setStyle("-fx-background-color: #336699;");
+    private void setTopComponenets(){
         titleAndBanner.getChildren().addAll(menuTitle, banner);
         titleAndBanner.setAlignment(Pos.CENTER);
+        //Temp cs style change to allow me to see the width of the banner and title*/
+        titleAndBanner.setStyle("-fx-background-color: #336699;");
+        this.setTop(titleAndBanner);
     }
 
+    private void setCenterComponenets() {
+        popularAndBrowse.getChildren().addAll(popularSelections, browseButton);
+        popularAndBrowse.setPrefHeight(this.getHeight());
+        GridPane.setHgrow(popularSelections, Priority.ALWAYS);
+        GridPane.setVgrow(browseButton, Priority.ALWAYS);
+        browseButton.setMinWidth(popularSelections.getWidth());
+        popularSelections.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        popularSelections.setPadding(new Insets(5,5,5,5));
+        popularAndBrowse.setPadding(new Insets(5, 5, 5, 5));
+        popularAndBrowse.setAlignment(Pos.CENTER);
 
-    private void setBrowseAndPopular(){
-        Rectangle popularContent = new Rectangle(200, 200, Color.LIGHTSTEELBLUE);
-        Tab popularTab = new Tab("Popular");
-        popularTab.setContent(popularContent);
-        popularTab.setClosable(false);
-        popularSelections.getTabs().add(popularTab);
-
-        browseButton.setPrefWidth(popularContent.getWidth());
-        //Add the components to the browseAndPopular VBox*/
-        browseAndPopular.getChildren().addAll(popularSelections, browseButton);
-        popularSelections.setMinWidth(this.getWidth()/2);
+        this.setCenter(popularAndBrowse);
 
     }
 
-
-    private void setCurrentlyPlaying(){
-
+    private void setRightComponenets(){
         //2D rectangle that serves as the view size for the currently playing image
         Rectangle2D viewportRect = new Rectangle2D(100, 100, 100, 100);
         //change the viewport to the rectangle
@@ -103,21 +108,18 @@ public class MainMenu extends VBox {
         currentlyPlayingImage.setPreserveRatio(true);
         currentlyPlayingImage.setSmooth(true);
         currentlyPlayingImage.setCache(true);
-
         Image dummyAlbumCover = new Image(getClass().getResourceAsStream("Images/display.png"));
         currentlyPlayingImage.setImage(dummyAlbumCover);
-        //Add all the components to the currently playing VBox*/
-        currentlyPlaying.getChildren().addAll(currentlyPlayingImage, currentlyPlayingInfo, songQueue);
 
-        currentlyPlaying.setPrefHeight(browseAndPopular.getHeight());
-        //Temp css color change to currently playing Image to see it on the screen
-        currentlyPlayingImage.setStyle("-fx-background-color: #993399;");
-
+        currentlyPlayingPortion.setAlignment(Pos.CENTER);
+        currentlyPlayingPortion.getChildren().addAll(currentlyPlayingImage, currentlyPlayingInfo, songQueue);
+        currentlyPlayingPortion.setPadding(new Insets(5, 15, 5, 5));
+        this.setRight(currentlyPlayingPortion);
     }
 
 
-
-
-
-
+    private void setLeftComponents(){
+        adminButton.setPadding(new Insets(5, 5, 5, 5));
+        this.setLeft(adminButton);
+    }
 }
